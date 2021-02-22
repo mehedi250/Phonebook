a0d04db97a95e988a65539e812a4d9c985a56d4a
mehedi250 <mhshawon250@gmail.com>
2021-02-22 15:28:31 +0600

    create master

--- resources/views/pages/master/master.blade.php
+++ resources/views/pages/master/master.blade.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        
         @yield('styles')
     
     </head>
@@ -23,6 +24,7 @@
         @yield('scripts')
 
         @include('pages.layouts.footer');
+
 
 
     </body>
